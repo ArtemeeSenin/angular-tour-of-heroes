e82bbb7d8935e4cc08c1ec614ba6fd0f51113ebb
Artem Senin <artem.senin@t-systems.com>
2019-01-22 17:21:49 +0300

    add heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Commando'},
+    { id: 12, name: 'Celeritas'},
+    { id: 13, name: 'Dynama'},
+    { id: 14, name: 'Dr QA'},
+    { id: 15, name: 'Bombasto'},
+    { id: 16, name: 'RubberMan'},
+    { id: 17, name: 'Tornado'},
+    { id: 18, name: 'LightArrow'},
+    { id: 19, name: 'Swirley'},
+    { id: 20, name: 'Magma'},
+    { id: 21, name: 'Mr. Nice'}
+]
